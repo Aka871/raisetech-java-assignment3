eea9268ada1ecb92aff27d0f2de31d3740e675c9
Aka871 <163530203+Aka871@users.noreply.github.com>
2024-05-14 23:58:03 +0900

    set up a projct
    Please enter the commit message for your changes. Lines starting

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> shopMap = new HashMap<>();
+        shopMap.put("Uniqlo", "3F-2");
+        shopMap.put("DAISO", "2F-5");
+        shopMap.put("McDonald’s", "1F-3");
+        shopMap.put("Supermarket", "1F-1");
+
+        System.out.println(shopMap.get("Uniqlo"));
+        System.out.println(shopMap.get("DAISO"));
+        System.out.println(shopMap.get("McDonald’s"));
+        System.out.println(shopMap.get("Supermarket"));
+
+        shopMap.remove("McDonald’s");
+        System.out.println(shopMap.get("McDonald’s"));
+
+        if (shopMap.containsKey("Uniqlo")) {
+            System.out.println("There is a Uniqlo.");
+        } else {
+            System.out.println("No Uniqlo");
+        }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the shop name: ");
+        String shopName = scanner.nextLine();
+        try {
+            if (shopMap.containsKey(shopName)) {
+                System.out.println(shopMap.get(shopName));
+            } else {
+                throw new ShopNotFoundException("Shop not found.");
+            }
+        } catch (ShopNotFoundException e) {
+            System.out.println("Not found.");
+        }
+    }
+}
